48914e0cdfc9eb4d5f911d00bd9dd2a1a03b7dab
mirage335 <spamfreemirage335@gmail.com>
2021-11-01 20:15:31 -0400

    Correction.

---
+++ _prog/specglobalvars.sh
@@ -0,0 +1,7 @@
+
+
+export ub_anchor_autoupgrade="true"
+
+#export ub_anchor_specificSoftwareName="experimental"
+export ub_anchor_user="true"
+
--- ubiquitous_bash.sh
+++ ubiquitous_bash.sh
@@ -32,7 +32,7 @@ _ub_cksum_special_derivativeScripts_contents() {
 #export ub_setScriptChecksum_disable='true'
 ( [[ -e "$0".nck ]] || [[ "${BASH_SOURCE[0]}" != "${0}" ]] || [[ "$1" == '--profile' ]] || [[ "$1" == '--script' ]] || [[ "$1" == '--call' ]] || [[ "$1" == '--return' ]] || [[ "$1" == '--devenv' ]] || [[ "$1" == '--shell' ]] || [[ "$1" == '--bypass' ]] || [[ "$1" == '--parent' ]] || [[ "$1" == '--embed' ]] || [[ "$0" == "/bin/bash" ]] || [[ "$0" == "-bash" ]] || [[ "$0" == "/usr/bin/bash" ]] || [[ "$0" == "bash" ]] ) && export ub_setScriptChecksum_disable='true'
 export ub_setScriptChecksum_header='1891409836'
-export ub_setScriptChecksum_contents='4199099508'
+export ub_setScriptChecksum_contents='2184935567'
 
 # CAUTION: Symlinks may cause problems. Disable this test for such cases if necessary.
 # WARNING: Performance may be crucial here.
@@ -17328,6 +17328,13 @@ _vars_vmMemoryAllocationDefault
 export hostToGuestDir="$instancedVirtDir"/htg
 export hostToGuestFiles="$hostToGuestDir"/files
 export hostToGuestISO="$instancedVirtDir"/htg/htg.iso 
+
+
+
+export ub_anchor_autoupgrade="true"
+
+#export ub_anchor_specificSoftwareName="experimental"
+export ub_anchor_user="true"
 
 
 
